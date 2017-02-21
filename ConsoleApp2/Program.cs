9792dc31ad1d00053671e33aa8d143c0d33ac883
Michał <kontakt@testingplus.me>
2017-02-21 19:52:15 +0100

    add ;) to code

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp2
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("; )");
         }
     }
 }
